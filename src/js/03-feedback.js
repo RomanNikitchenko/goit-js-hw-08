a2364e70ce76d471159e60f91c40abf498333f7d
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-01-26 16:57:00 +0200

    ht 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_kEY = 'valueInput';
+
+const refs = {
+  form: document.querySelector('.js-feedback-form'),
+  textarea: document.querySelector('.js-feedback-form  textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit)
+
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 1000))
+
+populateTextarea()
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_kEY);
+}
+
+
+function onTextareaInput(evt) {
+    const massage = evt.target.value
+
+    localStorage.setItem(STORAGE_kEY, massage)
+}
+
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_kEY)
+
+    if (savedMessage) {
+        refs.textarea.value = savedMessage;
+    }
+}
