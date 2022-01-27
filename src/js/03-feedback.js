246b302d3988ca9f4bc5a3793c402805c143bcc6
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-01-27 17:05:07 +0200

    th

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,21 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_kEY = 'valueInput';
+const STORAGE_kEY = 'feedback-form-state';
 
 const refs = {
-  form: document.querySelector('.js-feedback-form'),
-  textarea: document.querySelector('.js-feedback-form  textarea'),
-};
+    form: document.querySelector('.js-feedback-form'),
+    textarea: document.querySelector('.js-feedback-form  textarea'),
+    input: document.querySelector('.js-feedback-form  input'),
+}
 
-refs.form.addEventListener('submit', onFormSubmit)
+refs.form.addEventListener('submit', onFormSubmit);
 
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 1000))
-
-populateTextarea()
-
+populateTextarea();
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-
     evt.currentTarget.reset();
-
     localStorage.removeItem(STORAGE_kEY);
-}
-
-
-function onTextareaInput(evt) {
-    const massage = evt.target.value
-
-    localStorage.setItem(STORAGE_kEY, massage)
 }
 
 
@@ -34,6 +23,28 @@ function populateTextarea() {
     const savedMessage = localStorage.getItem(STORAGE_kEY)
 
     if (savedMessage) {
-        refs.textarea.value = savedMessage;
+
+        const jsonParse = JSON.parse(savedMessage);
+
+        if (jsonParse.email && jsonParse.message) {
+            refs.input.value = jsonParse.email;
+            refs.textarea.value = jsonParse.message;
+        } else if (jsonParse.email) {
+            refs.input.value = jsonParse.email;
+        } else if (jsonParse.message) {
+            refs.textarea.value = jsonParse.message;
+        }
+
     }
 }
+
+
+const formData = {};
+
+refs.form.addEventListener('input', (e) => {
+    formData[e.target.name] = e.target.value;
+
+    const formDataJSON = JSON.stringify(formData);
+
+    localStorage.setItem(STORAGE_kEY, formDataJSON);
+})
