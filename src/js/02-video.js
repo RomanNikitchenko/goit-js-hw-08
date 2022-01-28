b784cda8fbb502b1fdfa960c3439a1fc4ef89df6
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-01-28 17:06:37 +0200

    ht 8 t2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+import vimeoPlayer from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+    console.log(data.seconds);
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+
+player.on('timeupdate', onPlay);
+
+player.getCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function () {
+    
+});
