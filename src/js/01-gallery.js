7995c5ee5081338339821c3cfdb27858ca1f81bc
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-01-30 21:16:35 +0200

    ht 8 t1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,25 +4,25 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import imageCardTemplate from '../templates/picture-card.hbs';
 import { galleryItems } from './gallery-items.js';
 
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 
 const cardsMarcup = createImageCardsMarcup(galleryItems);
 
 function createImageCardsMarcup(Items) {
-    return Items.map(Item => imageCardTemplate(Item)).join("");
-};
+  return Items.map(Item => imageCardTemplate(Item)).join('');
+}
 
-gallery.insertAdjacentHTML("beforeend", cardsMarcup);
+gallery.insertAdjacentHTML('beforeend', cardsMarcup);
 
-gallery.addEventListener('click', (evt) => {
-    evt.preventDefault();
+gallery.addEventListener('click', evt => {
+  evt.preventDefault();
 
-    if (!evt.target.classList.contains("gallery__image")) {
-        return 
-    };
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
 });
 
 new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
